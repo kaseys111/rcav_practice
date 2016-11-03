24c6f0575aca0ff844874fb52d3886884dfe6aaa
Kasey Stonehill <kaseys111@users.noreply.github.com>
2016-11-03 13:29:07 -0500

    calculations complete

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,5 +1,26 @@
 class CalculationsController < ApplicationController
   def instructions
-    render("instructions.html.erb")
+  end
+
+  def square
+    @number = params[:number].to_f
+  end
+
+  def square_root
+    @number = params[:number].to_f
+  end
+
+  def random
+    @max = params[:max].to_i
+    @min = params[:min].to_i
+  end
+
+  def payment
+    @interest = (params[:interest].to_f)/100
+    @r = @interest/1200
+    @year = params[:year].to_i
+    @n = @year*12
+    @principal = params[:principal].to_f
+    @monthly_payment = (((@r*(@r+1)**@n)/((1+@r)**@n - 1))*@principal)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  get("/square/:number", { :controller => "calculations", :action => "square"})
+
+  get("/square_root/:number", { :controller => "calculations", :action => "square_root"})
+
+  get("/random/:min/:max", { :controller => "calculations", :action => "random"})
+
+  get("/payment/:interest/:year/:principal", { :controller => "calculations", :action => "payment"})
 end
